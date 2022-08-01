b972b384532fd2e8d56e07ba4accf8416056c80f
henriquegreve <xb9gx5zs1>
2022-08-01 18:01:03 -0300

    Teste

--- src/main/java/com/greve/spring/producer/configuration/ProducerRabbitConfiguration.java
+++ src/main/java/com/greve/spring/producer/configuration/ProducerRabbitConfiguration.java
@@ -51,6 +51,7 @@ public class ProducerRabbitConfiguration {
 
     @Bean
     public Binding bindingDeadLetter() {
+
         return BindingBuilder.bind(deadLetter())
                 .to(exchange())
                 .with(deadLetter);
